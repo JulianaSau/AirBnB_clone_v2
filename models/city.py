67e01d952e6bc72c853839dd6041d524071e621a
Sau Juliana <julienyange@gmail.com>
2022-09-13 02:18:58 +0300

    feat(place.py): add orm, create r/ship with user & city

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -12,6 +13,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         name = Column(String(128), nullable=False)
         
+        
+        places = relationship("Place", backref='cities' cascade='all, delete, delete-orphan')
 
 else:
     class City(BaseModel):
