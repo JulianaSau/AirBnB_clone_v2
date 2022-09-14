951fdf40558170130308538abe38433797530076
Adam Keino <keinoadam@gmail.com>
2022-09-14 16:16:37 +0000

    littu changes

--- models/user.py
+++ models/user.py
@@ -12,18 +12,18 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True, default='')
-        last_name = Column(String(128), nullable=True, default='')
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         # If the User object is deleted, all linked Place objects must be
         # automatically deleted
         places = relationship(
             "Place",
-            backref='user',
-            cascade='all, delete, delete-orphan')
+            backref="user",
+            cascade="all, delete, delete-orphan")
         reviews = relationship(
             "Review",
-            backref='user',
-            cascade='all, delete, delete-orphan')
+            backref="user",
+            cascade="all, delete, delete-orphan")
 else:
     class User(BaseModel):
         """This class defines a user by various attributes"""
