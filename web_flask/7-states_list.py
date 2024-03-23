c9782a428364214fc0718999f248a99ff448229e
Julie Sau <julienyange@gmail.com>
2024-03-23 20:09:30 +0300

    add task 7 that fetches all states and displays them

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module to start a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Renders “6-number_odd_or_even.html” template only if n is an integer, 
+        and shows whether odd/even
+        Args:
+            n (int): The number to check
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Closes the session after each request"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
